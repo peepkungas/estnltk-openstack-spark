50ecb71964eda078317cb44eb9b5d97d87085544
Simmo Siil <simmo.siil@zerotech.ee>
2016-03-02 15:26:30 +0200

    REG-550. Cron script for warc_to_hdfs

---
+++ warc_to_hdfs/scripts/warc_to_hdfs_execution.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#if script work is not in progress
+if [ $(ps aux | grep 'import_to_hdfs' | grep -v 'grep' | wc -l) -eq 0 ];
+then
+        for n in "$@"
+        do
+        case $n in
+                -p=*|--path=*)
+                path="${n#*=}"
+                ;;
+                -f=*|--file=*)
+                file="${n#*=}"
+                ;;
+        esac
+        done
+
+        if [ ! -d "$path" ];
+        then 
+               echo "Warc files path '$path' not found."
+               echo "Exiting"
+               exit
+        fi
+
+        if [ ! -f "$file" ];
+        then 
+               echo "nutchwax execution file '$file' not found."
+               echo "Exiting"
+               exit
+        fi
+
+        answer=`/bin/sh $file import_to_hdfs -p $path`
+        echo "$answer"
+        if [ -z "$answer" -a "$answer" != " " ];
+        then
+                echo "All files already successfully processed. Exiting."
+                exit
+        fi
+
+        echo "Successfully finished"
+else
+        echo "import_to_hdfs is running. Exiting"
+fi
