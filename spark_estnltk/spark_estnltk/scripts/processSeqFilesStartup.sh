2b7f93deb79a6a105438b01248e53d0f0de4dd4f
Simmo Siil <simmo.siil@zerotech.ee>
2016-04-04 16:09:53 +0300

    Executable script filename fix.

--- spark_estnltk/spark_estnltk/scripts/processSeqFilesStartup.sh
+++ spark_estnltk/spark_estnltk/scripts/processSeqFilesStartup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $(ps aux | grep 'processtest.sh' | grep -v 'grep' | wc -l) -lt 2 ];
 then
-  answer=`/bin/sh /opt/estnltk-openstack-spark/spark_estnltk/spark_estnltk/scripts/processtest.sh | tee -a /opt/logs/process-sequencefiles.log`
+  answer=`/bin/sh /opt/estnltk-openstack-spark/spark_estnltk/spark_estnltk/scripts/processSequencefiles.sh | tee -a /opt/logs/process-sequencefiles.log`
   echo $answer
   if [ -z "$answer" -a "$answer" != " " ]; then
     echo "All files processed. Exiting" >> /opt/logs/process-sequencefiles.log
